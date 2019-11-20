130e6f09d989b31ef818389d376a92e1d0e5f6ce
Philipp Dorner <pdorner@gmx.net>
2019-11-20 12:02:09 +0100

    asdf

---
+++ dayThree/src/module-info.java
@@ -0,0 +1,11 @@
+module dayThree {
+	requires javafx.base; 
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.graphics;
+	requires javafx.swing;
+	requires javafx.media;
+	requires javafx.swt;
+	requires javafx.web;
+	exports dayThree;
+}
---
+++ dayTwo/src/dayTwo/ExerciseTwo.java
@@ -0,0 +1,16 @@
+package dayTwo;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ExerciseTwo extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
